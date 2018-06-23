72e8777ac138d86cd554b0b7f828c30be888b02e
aymericq <aquesne@gmail.com>
2018-06-23 13:01:45 +0200

    Added automated response when webhook receives a message.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from pymongo import MongoClient
 import os
 from flask import Flask, request, render_template
+import requests
+
 app = Flask(__name__)
 
 client = MongoClient(os.environ.get('MONGODB_URL'))
+VERIFY_TOKEN = os.environ.get('FB_VERIFY_TOKEN')
+PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 db = client.rmdr
 
 @app.route("/")
@@ -12,7 +16,6 @@ def hello():
 
 @app.route("/webhook", methods=['GET', 'POST'])
 def webhook():
-    VERIFY_TOKEN = "NSjntmHrbzysHOEX6F4DrG6P4zAprPUxdHpsUKyR"
     if request.method == 'GET':
         if('hub.mode' in request.args and 'hub.verify_token' in request.args and 'hub.challenge' in request.args):
             mode = request.args['hub.mode']
@@ -29,8 +32,25 @@ def webhook():
         db.logs.insert_one(body)
         if(body.get('object') == 'page'):
             for entry in body.get('entry'):
-                app.logger.warning(entry.get('messaging')[0].get('message'))
+                for message in entry.get("messaging"):
+                    handleMessage(messaging.message, messaging.sender.id)
             return 'EVENT_RECEIVED'
         else:
             '', 404
     return '', 403
+
+def handleMessage(message, sender_psid):
+    if text in message:
+        res = {
+            "text" : "Vous avez envoyé : '" + message.get('text') + "'."
+        }
+        callSendAPI(res, sender_psid)
+
+def callSendAPI(sender_psid, res):
+    request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": req
+    }
+    r = requests.post('https://graph.facebook.com/v2.6/me/messages?access_token='+PAGE_ACCESS_TOKEN, request_body)
