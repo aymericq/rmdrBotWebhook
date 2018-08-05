a34d5a8daf5c8b26796ac32018910a8596d37021
aymericq <aquesne@gmail.com>
2018-08-05 17:43:45 +0200

    Factorized hello response

--- app.py
+++ app.py
@@ -49,40 +49,7 @@ def handle_message(message, sender_psid):
         state =  db.users.find_one({"psid" : sender_psid}, {"_id" : 0, "state" : 1}).get('state')
         print(state)
         if message.get('text').lower().find("bonjour") != -1:
-            # TODO : recup les infos de profil avec < https://graph.facebook.com/v2.6/<PSID>?fields=first_name,last_name,profile_pic&access_token=<PAGE_ACCESS_TOKEN>" >
-            r = requests.get("https://graph.facebook.com/v2.6/{}?fields=first_name,last_name&access_token={}".format(sender_psid, PAGE_ACCESS_TOKEN))
-            body = r.json()
-            resp_text = "Bonjour {}.".format(body.get("first_name"))
-            quick_replies = []
-            if db.users.count({"psid" : sender_psid}) >= 1:
-                resp_text += "\nBienvenue à nouveau parmi nous ! :)"
-                resp_text += "\nQu'est-ce qui t'amène ?"
-                quick_replies = [
-                    {
-                        "content_type":"text",
-                        "title":"Ajout film vu",
-                        "payload":"ADD_SEEN_MOVIE"
-                    },
-                    {
-                        "content_type":"text",
-                        "title":"Ajout envie",
-                        "payload":"ADD_WISH"
-                    }
-                ]
-            else:
-                user = {
-                    "first_name" : body.get("first_name"),
-                    "last_name" : body.get("last_name"),
-                    "psid" : sender_psid,
-                    "films" : [],
-                    "state" : "HELLO"
-                }
-                db.users.insert_one(user)
-            db.users.update({"psid" : sender_psid}, {"$set":{"state" : "HELLO"}})
-            res = {
-                "text" : resp_text,
-                "quick_replies" : quick_replies
-            }
+            res = hello(sender_psid)
             call_send_API(res, sender_psid)
         elif 'quick_reply' in message:
             payload = message.get('quick_reply').get('payload')
@@ -243,3 +210,38 @@ def add_movie(mov_info, sender_psid, status):
             "film_id" : inserted_id
         }}})
         return 0
+
+def hello(sender_psid):
+    r = requests.get("https://graph.facebook.com/v2.6/{}?fields=first_name,last_name&access_token={}".format(sender_psid, PAGE_ACCESS_TOKEN))
+    body = r.json()
+    resp_text = "Bonjour {}.".format(body.get("first_name"))
+    if db.users.count({"psid" : sender_psid}) >= 1:
+        resp_text += "\nBienvenue à nouveau parmi nous ! :)"
+        resp_text += "\nQu'est-ce qui t'amène ?"
+    else:
+        user = {
+            "first_name" : body.get("first_name"),
+            "last_name" : body.get("last_name"),
+            "psid" : sender_psid,
+            "films" : [],
+            "state" : "HELLO"
+        }
+        db.users.insert_one(user)
+    quick_replies = [
+        {
+            "content_type":"text",
+            "title":"Ajout film vu",
+            "payload":"ADD_SEEN_MOVIE"
+        },
+        {
+            "content_type":"text",
+            "title":"Ajout envie",
+            "payload":"ADD_WISH"
+        }
+    ]
+    db.users.update({"psid" : sender_psid}, {"$set":{"state" : "HELLO"}})
+    res = {
+        "text" : resp_text,
+        "quick_replies" : quick_replies
+    }
+    return res
